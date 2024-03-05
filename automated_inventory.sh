ea63e3cac6681d07bd48fab7c76d7eab56f085c9
TopChainBoy <topchainboy@thedaoma.com>
2024-03-06 00:42:04 +0100

    Improved network scanning script by enhancing command checks, refining command outputs, and ensuring safe reading of IP addresses. Also, made the list of required commands more maintainable by storing them in an array.

--- automated_inventory.sh
+++ automated_inventory.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
-BASE_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+BASE_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &>/dev/null && pwd )"
 
 # Function to check if a command exists
-function command_exists() {
-  command -v "$1" >/dev/null 2>&1
+command_exists() {
+  command -v "$1" &>/dev/null
 }
 
 # Function to display usage information
-function usage() {
+usage() {
   echo "Usage: $0 [-h|--help]"
   echo "Scan the network and get information about each device."
   echo "Options:"
@@ -22,35 +22,36 @@ if [[ "$1" == "-h" || "$1" == "--help" ]]; then
 fi
 
 # Check if required commands are installed
-for cmd in nmap arp sshpass ssh ipcalc; do
+REQUIRED_COMMANDS=("nmap" "arp" "sshpass" "ssh" "ipcalc")
+for cmd in "${REQUIRED_COMMANDS[@]}"; do
   if ! command_exists $cmd; then
     echo "The command '$cmd' is not installed. Install it by running 'sudo apt install $cmd'."
     exit 1
   fi
 done
 
-# Define the network range MANUAL METHOD
-#network="192.168.1.0/24"
-
 # Get the IP address and subnet mask of the active network interface
 ip_info=$(ip -o -f inet addr show | awk '/scope global/ {print $4}')
+
+# Define the network range MANUAL METHOD
+#network="192.168.1.0/24"
 
 # Use ipcalc to get the network range
 network=$(ipcalc -n $ip_info | awk -F= '/Network/ {print $2}')
 
 # Use nmap to scan the network
-nmap -sn $network | grep 'Nmap scan report for' | awk '{print $5}' > $BASE_DIR/ip_addresses.txt
+nmap -sn $network | awk '/Nmap scan report for/ {print $5}' > $BASE_DIR/ip_addresses.txt
 
 # Loop through each IP address
-while read ip; do
+while read -r ip; do
   echo "IP Address: $ip"
 
   # Get the MAC address
-  mac=$(arp -n $ip | grep ether | awk '{print $3}')
+  mac=$(arp -n $ip | awk '/ether/ {print $3}')
   echo "MAC Address: $mac"
 
   # Get the device type and operating system using nmap
-  os=$(nmap -O $ip | grep 'Running:' | cut -d: -f2)
+  os=$(nmap -O $ip | awk '/Running:/ {print substr($0, index($0, $2))}')
   echo "Operating System: $os"
 
   # Get hardware specifications using ssh and dmidecode (requires ssh access)
